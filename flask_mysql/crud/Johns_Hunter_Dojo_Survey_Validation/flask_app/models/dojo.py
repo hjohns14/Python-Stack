dbebffd98172c6b11c90a5e0197b87d39befe991
Hunter Johns <hjohns1737@gmail.com>
2022-07-11 12:43:04 -0600

    Added Email regex

--- flask_mysql/crud/Johns_Hunter_Dojo_Survey_Validation/flask_app/models/dojo.py
+++ flask_mysql/crud/Johns_Hunter_Dojo_Survey_Validation/flask_app/models/dojo.py
@@ -1,5 +1,19 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
+import re	# the regex module
+# create a regular expression object that we'll use later
+""" 
+This is my understanding of the email regex. I may be wrong about some parts
+
+^ ->starts with
+[] -> set of characters to look for
+[a-zA-Z0-9.+_-]+ -> all characters repeated any number of times
+@ -> check for @
+\. -> escape + .  (the dot in .com)
+[a-zA-Z]+ -> a-z lower and upper any number of time (the com)
+$ -> at the end of the string
+"""
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 
 class Dojo:
     db_name = "dojo_survey_schema"
